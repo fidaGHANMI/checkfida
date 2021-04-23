32bef2619804dbafb6863e7c0ca04c5a371d3778
fida ghanmi <fidaghanmi@gmail.com>
2021-04-23 21:39:10 +0100

    final project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import adress from "./components/profile/Adress"
-import profilePhoto from "./components/profile/ProfilePhoto"
+import Adress from "./components/profile/Adress"
+import ProfilePhoto from "./components/profile/ProfilePhoto"
 import FullName from "./components/profile/FullName"
 
 
 function App() {
   return (
     <div className="App">
-      <ProfilePhoto/>
+      
       <FullName/>
       <Adress/>
+       <ProfilePhoto/>
+
     </div>
   );
 }
